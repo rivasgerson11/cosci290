1b559af9b7620c472ccadc982b33ebfbfd3f9ae2
rivasgerson11 <rivasgerson11@gmail.com>
2018-02-22 02:32:41 -0500

    zombie Game Driver

--- GameDriver.java
+++ GameDriver.java
@@ -18,16 +18,22 @@ public class GameDriver{
     
     
   //splash screen  
-  System.out.println( "  _|_|_|                                                    \n"
-                    + " _|          _|_|    _|  _|_|    _|_|_|    _|_|    _|_|_|  \n"  
-                    + " _|  _|_|  _|_|_|_|  _|_|      _|_|      _|    _|  _|    _| \n"  
-                    + " _|    _|  _|        _|            _|_|  _|    _|  _|    _| \n"  
-                    + "   _|_|_|    _|_|_|  _|        _|_|_|      _|_|    _|    _|  ");
+  System.out.println( "   @@@@@@@@   @@@@@@   @@@@@@@@@@   @@@@@@@   @@@  @@@@@@@@   @@@@@@  \n" 
+                  +  "    @@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@  @@@@@@@@  @@@@@@@  \n"  
+                  +  "         @@!  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!  @@!       !@@   \n"     
+                  +  "        !@!   !@!  @!@  !@! !@! !@!  !@   @!@  !@!  !@!       !@!   \n"     
+                  +  "       @!!    @!@  !@!  @!! !!@ @!@  @!@!@!@   !!@  @!!!:!    !!@@!!  \n"   
+                  +  "      !!!     !@!  !!!  !@!   ! !@!  !!!@!!!!  !!!  !!!!!:     !!@!!!   \n" 
+                  +  "     !!:      !!:  !!!  !!:     !!:  !!:  !!!  !!:  !!:            !:!  \n" 
+                  +  "    :!:       :!:  !:!  :!:     :!:  :!:  !:!  :!:  :!:           !:!   \n" 
+                  +  "     :: ::::  ::::: ::  :::     ::    :: ::::   ::   :: ::::  :::: ::   \n" 
+                  +  "    : :: : :   : :  :    :      :    :: : ::   :    : :: ::   :: : :  \n"  ); 
+                                                                     
     
   //story setup
   System.out.println(" Soldier: Hey there survivor! ");
   
-  System.out.println(" Survivor: Hey there! ") 
+  System.out.println(" Survivor: Hey there! "); 
     
   System.out.println(" Soldier: What is your name? ");
   
@@ -35,28 +41,62 @@ public class GameDriver{
     
   System.out.println(" Soldier: Wow " + name + " I can't believe you're still alive! ");
     
-  Sysyem.out.println(" Survivor: It has been a struggle trying to staying alive, but I've been holding them zombies back "); 
+  System.out.println( name +": It has been a struggle trying to staying alive, but I've been holding them zombies back "); 
     
   System.out.println(" Soldier:  How many days have you been stranded by yourself on this island? ");
     
   int days = input.nextInt();
     
-  System.out.println(" Survivor: I've kept myself alive for " + days + " days. ")  
+  System.out.println( name +": I've kept myself alive for " + days + " days. ");  
     
   System.out.println(" Soldier: Great!  ");
     
-  System.out.println(" Soldier: Well you're the last survivor, so we must now get back to the chopper! ")
+  System.out.println(" Soldier: Well you're the last survivor, so we must now get back to the chopper! ");
     
-  System.out.println(" Survivor: But, we must fight our way through! ")  
+  System.out.println( name +": But, we must fight our way through! ");  
     
-  System.out.println(" Soldier: Yes, take this weapon and we might have a chance to make it through. ")
+  System.out.println(" Soldier: Yes, take this weapon and we might have a chance to make it through. ");
+    
+  System.out.println( name +": Lets get to the chopper! ");
+    
+  
+  System.out.println("========================================== zombies loading ==========================================");  
+    
+    
+    
     
   int numberOfZombies = 20;
   int actNumberOfZombies = 100;
   
-  chanceOfSurviving = 8 + (int)(Math.random() * 100 )  
+  int chanceOfSurviving = 8 + (int)(Math.random() * 100 );  
     
-  if(chanceOfSurviving <= 25 )  
+  if(chanceOfSurviving <= 25 ){
+    System.out.println("You made it through the " + chanceOfSurviving + " zombies alive and safely got to the chopper! ");
+  }  
+  else{
+    System.out.println(" The " + chanceOfSurviving + " zombies ate your brains, you died ");
+    
+  System.out.println(  "   _|_|_|  \n"                                    
+                   +  "   _|          _|_|_|  _|_|_|  _|_|      _|_|  \n"  
+                   +  "   _|  _|_|  _|    _|  _|    _|    _|  _|_|_|_|  \n"
+                   +  "   _|    _|  _|    _|  _|    _|    _|  _|        \n"
+                   +  "     _|_|_|    _|_|_|  _|    _|    _|    _|_|_|  \n"
+
+
+
+                    + "     _|_|   \n"                                 
+                    +  "  _|    _|  _|      _|    _|_|    _|  _|_| \n" 
+                    + "   _|    _|  _|      _|  _|_|_|_|  _|_|  \n"    
+                    + "   _|    _|    _|  _|    _|        _|  \n"      
+                    + "     _|_|        _|        _|_|_|  _|   \n" );     
+                                           
+    
+    
+    
+    
+  }   
+    
+    
   }
   
   
